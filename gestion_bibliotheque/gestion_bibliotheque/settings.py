338041dce4af4ee7fe37690ef4ca4529e6545acc
enamya007 <balakiyemkola@gmail.com>
2026-03-24 10:07:18 +0000

    I'd edit settings.py

--- gestion_bibliotheque/gestion_bibliotheque/settings.py
+++ gestion_bibliotheque/gestion_bibliotheque/settings.py
@@ -122,5 +122,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Pour les fichiers statiques (CSS, JS)
 STATIC_URL = '/static/'
